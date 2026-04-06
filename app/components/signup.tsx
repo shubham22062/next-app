5042372d443324a118cea8b41c45731972beb845
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-06 21:18:23 +0530

    datbase

---
+++ app/components/signup.tsx
@@ -0,0 +1,12 @@
+export default function signup(){
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder="enter the username"/>
+            </div>
+            <div>
+                <input type = "text" placeholder="enter the password"/>
+            </div>
+        </div>
+    )
+}
