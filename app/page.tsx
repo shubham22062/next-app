94d743012a185ef1ac9d85be2bea64ba685474b0
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-05-10 17:28:45 +0530

    next-Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 import axios from "axios"
 
-async function getUserDetails(){
-  const respone = await axios.get(" http://localhost:3000/api/user")
-  return respone.data;
-}
+
 
 export default async function Home(){
-  const userData = await getUserDetails();
+
    return(
     <div className="flex flex-col justify-center h-screen  border-8 rounded-r-2xl">
     <div className="flex justify-center">
-      {userData.email};
+      email
       </div>
       <div className="flex justify-center">
-      {userData.username};
+      username
     </div>
     </div>
    )
